72a24531a93d08f855c2f6d57036f26a695ea6d2
Leaversa <mtkwong@cpp.edu>
2024-06-28 14:17:44 -0700

    Add support for vue components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,38 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'NGCP Ground Control Systems',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'User Interface',
-					autogenerate: { directory: 'user-interface' },
-				},
-				{
-					label: 'Databse & API',
-					autogenerate: { directory: 'database' },
-				},
-				{
-					label: 'Vehicle Integration',
-					autogenerate: { directory: 'vehicle-integration' },
-				},
-				{
-					label: 'Infrastucture',
-					autogenerate: { directory: 'infrastructure' },
-				},
-				{
-					label: 'Resources',
-					link: '/resources',
-				}
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'NGCP Ground Control Systems',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'User Interface',
+      autogenerate: {
+        directory: 'user-interface'
+      }
+    }, {
+      label: 'Databse & API',
+      autogenerate: {
+        directory: 'database'
+      }
+    }, {
+      label: 'Vehicle Integration',
+      autogenerate: {
+        directory: 'vehicle-integration'
+      }
+    }, {
+      label: 'Infrastucture',
+      autogenerate: {
+        directory: 'infrastructure'
+      }
+    }, {
+      label: 'Resources',
+      link: '/resources'
+    }]
+  }), vue()]
 });
