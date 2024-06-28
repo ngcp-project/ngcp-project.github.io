b9bdde45f459f6cf1c6cd00649066bec804602c3
Leaversa <mtkwong@cpp.edu>
2024-06-28 14:25:37 -0700

    Add example user interface component page

---
+++ src/content/docs/user-interface/components/test.component.vue
@@ -0,0 +1,23 @@
+<template>
+    <button @click="handleClick" class="button">{{ label }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        label: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        handleClick() {
+            // Handle button click event here
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
