1773baafabd33bf2dde195ea682ef97b386ee621
Leaversa <mtkwong@cpp.edu>
2024-06-28 14:25:12 -0700

    Fix home page redirect and add getting started page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,51 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'NGCP Ground Control Systems',
-    social: {
-      github: 'https://github.com/withastro/starlight'
-    },
-    sidebar: [{
-      label: 'User Interface',
-      autogenerate: {
-        directory: 'user-interface'
-      }
-    }, {
-      label: 'Databse & API',
-      autogenerate: {
-        directory: 'database'
-      }
-    }, {
-      label: 'Vehicle Integration',
-      autogenerate: {
-        directory: 'vehicle-integration'
-      }
-    }, {
-      label: 'Infrastucture',
-      autogenerate: {
-        directory: 'infrastructure'
-      }
-    }, {
-      label: 'Resources',
-      link: '/resources'
-    }]
-  }), vue()]
+	integrations: [
+		starlight({
+			title: 'NGCP Ground Control Systems',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Getting Started',
+					link: '/getting-started',
+				},
+				{
+					label: 'User Interface',
+					autogenerate: {
+						directory: 'user-interface',
+					},
+				},
+				{
+					label: 'Databse & API',
+					autogenerate: {
+						directory: 'database',
+					},
+				},
+				{
+					label: 'Vehicle Integration',
+					autogenerate: {
+						directory: 'vehicle-integration',
+					},
+				},
+				{
+					label: 'Infrastucture',
+					autogenerate: {
+						directory: 'infrastructure',
+					},
+				},
+				{
+					label: 'Resources',
+					link: '/resources',
+				},
+			],
+		}),
+		vue(),
+	],
 });
