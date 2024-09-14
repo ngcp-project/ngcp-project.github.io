111a5e4fea38694b838db540b56290eb0a72b90e
Leaversa <mtkwong@cpp.edu>
2024-09-13 18:10:34 -0700

    chore: update socials

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
 		starlight({
 			title: 'NGCP Ground Control Systems',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/ngcp-project',
 			},
 			sidebar: [
 				{
